d12e8ac2639a2a857503fd6fb40f0e86262f845d
nawpioon <nawpioon@gmail.com>
2023-01-07 11:31:35 +0700

    conn edit

--- conn.php
+++ conn.php
@@ -4,6 +4,11 @@ $username = "root";
 $password = "123456789";
 $dbname = "web2111";
 
+$servername = "localhost";
+$username = "it65309010003";
+$password = "it12345";
+$dbname = "it65309010003";
+
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // mysqli_set_charest($conn,"utf8");
